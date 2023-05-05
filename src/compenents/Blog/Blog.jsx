c551559659be11712d92936996132b1159e6e440
MuhammadAtiq757 <muhammadatiq757@gmail.com>
2023-05-05 16:50:22 +0600

    in pdf file added all question answer

--- src/compenents/Blog/Blog.jsx
+++ src/compenents/Blog/Blog.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import { jsPDF } from "jspdf";
-import logo from '../Blog/image/atiq.jpeg'
+// import logo from '../Blog/image/atiq.jpeg'
 
 
 const Blog = () => {
-   const pdfGenerate=()=>{
+    const pdfGenerate = () => {
         var doc = new jsPDF('landscape', 'px', 'a4', 'false');
-        doc.addImage(logo, "JPEG", 65, 20,500,400)
-        doc.addPage()
-        doc.text(60, 60,'Description')
-        doc.text(100, 60, `This is the blog page there have some importent question with answer. i think 
-        these qustion you should be know for your future. there has four diffrernt question. but these question is very easy`)
+        // doc.addImage(logo, "JPEG", 65, 20, 500, 400)
+        doc.text(40, 40, `
+        first qus ans: In software development, the terms "controlled" and
+        "uncontrolled" components typically refer to different ways of 
+        managing user input in a user interface. depends on the state of another.
+
+        second quse ans:js web application framework that provides a robust set
+        of features for web and mobile applications. In other words, NodeJS is
+        the package, which provides the JavaScript run-time environment, whereas Express is
+        a framework that sits on top of NodeJS and helps us to handle requests and responses.
+
+        third ques ans: React PropTypes validators
+        PropTypes.any : The prop can be of any data type.
+        PropTypes.bool : The prop should be a Boolean.
+        PropTypes.number : The prop should be a number.
+
+        last ques ans: Custom React JS hooks offer reusability as when a 
+        custom hook is created, and again while rendering the whole code.`)
         doc.save('atiq.pdf')
     }
 
@@ -41,12 +54,12 @@ const Blog = () => {
                         PropTypes.any : The prop can be of any data type.
                         PropTypes.bool : The prop should be a Boolean.
                         PropTypes.number : The prop should be a number.
-                        </p>
+                    </p>
 
                 </div>
                 <div className="card-body blog-body">
                     <h2 className="card-title">What is a custom hook, and why will you create a custom hook?</h2>
-                    <p>Custom React JS hooks offer reusability as when a 
+                    <p>Custom React JS hooks offer reusability as when a
                         custom hook is created, and again while rendering the whole code.</p>
 
                 </div>
